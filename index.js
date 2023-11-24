01d74c80e0ec0928cbea1bbdc9877480a6683aab
Rayyan <126807224+333Rayyan@users.noreply.github.com>
2023-11-24 14:28:27 +0000

    Refactor HTML code in views files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import the modules we need
-var express = require ('express')
+var express = require('express')
 var ejs = require('ejs')
-var bodyParser= require ('body-parser')
+var bodyParser = require('body-parser')
 var mysql = require('mysql');
 
 // Create the express application object
@@ -9,7 +9,7 @@ const app = express()
 const port = 8000
 app.use(bodyParser.urlencoded({ extended: true }))
 // Define the database connection
-const db = mysql.createConnection ({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'appuser',
     password: 'app2027',
@@ -41,7 +41,7 @@ app.set('view engine', 'ejs');
 app.engine('html', ejs.renderFile);
 
 // Define our data
-var shopData = {shopName: "Bertie's Books"}
+var shopData = { shopName: "Bertie's Books" }
 
 // Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
 require("./routes/main")(app, shopData);
--- routes/main.js
+++ routes/main.js
@@ -1,35 +1,94 @@
-module.exports = function(app, shopData) {
+// Exporting a function that defines routes for the web application
+module.exports = function (app, shopData) {
 
-    // Handle our routes
-    app.get('/',function(req,res){
-        res.render('index.ejs', shopData)
+    // Route for the home page
+    app.get('/', function (req, res) {
+        res.render('index.ejs', shopData);
     });
-    app.get('/about',function(req,res){
+
+    // Route for the about page
+    app.get('/about', function (req, res) {
         res.render('about.ejs', shopData);
     });
-    app.get('/search',function(req,res){
+
+    // Route for the search page
+    app.get('/search', function (req, res) {
         res.render("search.ejs", shopData);
     });
-    app.get('/search-result', function (req, res) {
-        //searching in the database
-        res.send("You searched for: " + req.query.keyword);
-    });
-    app.get('/register', function (req,res) {
-        res.render('register.ejs', shopData);                                                                     
-    });  
-    app.get('/list', function(req, res) {
-        let sqlquery = "SELECT * FROM books"; // query database to get all the books
-        // execute sql query
+
+    // Route for listing all books in the database
+    app.get('/list', function (req, res) {
+        let sqlquery = "SELECT * FROM books"; // SQL query to get all books from the database
+        // Execute the SQL query
         db.query(sqlquery, (err, result) => {
             if (err) {
-                res.redirect('./'); 
+                res.redirect('./'); // Redirect to the home page in case of an error
+            } else {
+                // Combine existing data with the result and render the list page
+                let newData = Object.assign({}, shopData, { availableBooks: result });
+                res.render("list.ejs", newData);
             }
-            res.send(result)
-         });
+        });
     });
-                                                                                               
-    app.post('/registered', function (req,res) {
-        // saving data in database
-        res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
-    }); 
+
+    // Route for listing bargain books (books with price less than 20)
+    app.get('/bargainbooks', function (req, res) {
+        let sqlquery = "SELECT * FROM books WHERE price < 20"; // SQL query to get bargain books from the database
+        // Execute the SQL query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./'); // Redirect to the home page in case of an error
+            } else {
+                // Combine existing data with the result and render the bargain books page
+                let newData = Object.assign({}, shopData, { availableBooks: result });
+                res.render("bargainbooks.ejs", newData);
+            }
+        });
+    });
+
+    // Route for the registration page
+    app.get('/register', function (req, res) {
+        res.render('register.ejs', shopData);
+    });
+
+    // Route for handling the registration form submission
+    app.post('/registered', function (req, res) {
+        // Saving data in the database (not implemented in the provided code)
+        res.send(' Hello ' + req.body.first + ' ' + req.body.last + ' you are now registered!  We will send an email to you at ' + req.body.email);
+    });
+
+    // Route for adding a new book to the database
+    app.get('/addbook', function (req, res) {
+        res.render('addbook.ejs', shopData);
+    });
+
+    // Route for handling the new book form submission
+    app.post('/bookadded', function (req, res) {
+        let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"; // SQL query to insert a new book into the database
+        // Execute the SQL query
+        let newrecord = [req.body.name, req.body.price];
+        db.query(sqlquery, newrecord, (err, result) => {
+            if (err) {
+                return console.error(err.message);
+            } else {
+                res.send(' This book is added to the database, name: '
+                    + req.body.name + ' price ' + req.body.price);
+            }
+        });
+    });
+
+    // Route for searching the database based on the entered keyword
+    app.get('/search-result', function (req, res) {
+        let sqlquery = "SELECT * FROM books WHERE name LIKE '%" + req.query.keyword + "%'"; // SQL query to search for books based on the keyword
+        // Execute the SQL query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./'); // Redirect to the home page in case of an error
+            } else {
+                // Combine existing data with the result and render the list page
+                let newData = Object.assign({}, shopData, { availableBooks: result });
+                res.render("list.ejs", newData);
+            }
+        });
+    });
 }
